0d182152bab4768a23172186cb4f9de20014314c
BillE <bill.emms064@gmail.com>
2025-10-06 09:45:54 -0400

    Object2

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -26,7 +26,9 @@ function showbooks() {
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>`
+
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -47,3 +49,9 @@ function editbook(index) {
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
  }
+
+ function deletebook(index) {
+    const book = books[index];    
+    books.splice(index, 1); // Remove old entry
+    showbooks(); // Refresh list
+  }
